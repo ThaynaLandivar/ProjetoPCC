27b7f8e2688250aac4986e49127ab50a443f9567
FabioFbn <152935743+FabioFbn@users.noreply.github.com>
2025-05-13 20:22:48 -0300

    13-05

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const routes = require("./routes/index");
-const { User, sequelize } = require('./models/User');
+const { User, sequelize } = require('./user');
 const { Sequelize } = require('sequelize');
 
 // Sincroniza o banco ao iniciar o app
